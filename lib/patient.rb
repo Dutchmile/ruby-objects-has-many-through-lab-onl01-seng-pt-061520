c654a713d80ea9695a80f6919f13b1188ed59c57
Christopher Dutcher <cadutcher@icloud.com>
2020-08-09 19:04:16 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,29 @@
+class Patient
+
+  attr_accessor :name
+  @@all_p << []
+
+  def initialize(name)
+    @name = name
+    @@all_p << self
+  end
+
+  def new_appointment(date, doctor)
+    Appointment.new(date, self, doctor)
+  end
+
+  def self.all
+    @@all_p
+  end
+
+  def appointments
+    Appointments.all.select {|a| a.patient == self}
+  end
+
+  def doctors
+    doctors = []
+    Appointments.all.select {|a| doctors << a if a.patient == self}
+    doctors
+  end
+
+end
