9961d40e315487ae1d61ba0eec59f5aa6414d8ad
Christopher Dutcher <cadutcher@icloud.com>
2020-08-09 18:51:25 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -2,11 +2,18 @@ class Genre
 
   attr_reader :name
 
+  @@all_g = []
+
   def initialize(name)
     @name = name
+    @@all_g << self
   end
 
   def songs
     Song.all.select {|s| s.genre == self}
+  end
+
+  def self.all
+    @@all_g
   end
 end
