c70bc1760a657b0ba517f02cf0f5b4a2b31a790e
Christopher Dutcher <cadutcher@icloud.com>
2020-08-09 18:27:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,9 +2,15 @@ class Song
 
   attr_reader :title, :artist, :genre
 
+  @@all_s = []
+
     def initialize(title, artist, genre)
       @title = title
       @artist = artist
       @genre = genre
+    end
+
+    def self.all
+      @@all_s
     end
 end
